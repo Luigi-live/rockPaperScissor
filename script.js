32f2c317a372128a5c1e9a08418e48652b8ae2f8
LuigiVetere <215154651+Luigi-live@users.noreply.github.com>
2025-09-17 17:45:13 -0300

    feature: logic for human choice

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 function getComputerChoise() {
-    let choise;
+    let computreChoise;
     let randomNumber = Math.floor(Math.random() * 100);
     console.log(randomNumber)
     if(randomNumber <= 33){
-        choise = "paper"
+        computerChoise = "paper"
     }
     else if (66 < randomNumber) {
-        choise = "scissor"
+        computerChoise = "scissor"
     }
     else if (33 < randomNumber <= 66){
-        choise = "rock"
+        computerChoise = "rock"
     }
-    return choise;
+    return computerChoise;
 };
 
-console.log(getComputerChoise());
+function getHumanChoise() {
+    console.log("For this game, we have 3 options: ")
+    console.log("1 - Paper")
+    console.log("2 - Rock")
+    console.log("3 - Scissor")
+    let humanChoise = prompt("What is your choise:")
+    while(humanChoise != "1" && humanChoise != "2" && humanChoise != "3"){
+        humanChoise = prompt("Come on, don't be shy. Chose a number(1 or 2 or 3):")
+    }
+    if(humanChoise === "1"){
+        humanChoise = "paper"
+    }
+    else if (humanChoise === "2"){
+        humanChoise = "rock"
+    }
+    else if (humanChoise === "3"){
+        humanChoise = "scissor"
+    }
+    
+
+    console.log(humanChoise)
+
+};
+
+console.log(getHumanChoise());
